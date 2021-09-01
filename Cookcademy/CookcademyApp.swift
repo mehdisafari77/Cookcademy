7c902da299079d760598936a85e5ddf891a4a2f8
Mehdi Safari <sa.mehdisafari@gmail.com>
2021-09-01 09:44:44 -0700

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Mehdi MMS on 01/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
